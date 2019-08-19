a7700f537be903dcc9f61f6e64ae44d2ac016ebc
Misty Malkasian <andrewandmisty@gmail.com>
2019-08-19 12:30:36 -0500

    initial upload with comments and basic framework

---
+++ diceRollGame.js
@@ -0,0 +1,52 @@
+
+function randomDiceValue (minSides, maxSides){
+        let dieRoll = Math.floor(Math.random(0)*(maxSides-minSides)) + minSides;
+        return dieRoll
+}
+
+function newGame() {
+
+   let startButton = onclick() // WILL WORK ON 
+}
+
+function startMenu (buttonOne, buttonTwo){
+
+        //if SinglePlayer run separate function of multiplayer
+}
+
+//this is a 40 yard course
+
+function DrivingMenu() { // between 40 - 20 yards
+
+    // 4 buttons that gray out on clicks (SWITCH CASE)
+    // case: 1 - die 20 - driver
+    // case: 2 - die 12 -  weak iron - looks like it can be used as a putter
+    // case: 3 - die 10 -  beat-up iron - looks like it can be used as a putter
+    // case: 4 - die 8 - wonky iron looks like it can be used as a putter
+    
+    
+}
+
+// possiblitiy to get into the sandpit between 25-10 yards
+function ifYouHitSandPit(){
+    // you pull out your die 4 and it let's them know the below switch case. 
+
+    // switch 
+    //case rolling a 1 = you hit the ball backward 10 yards.
+    //case rolling a 2 = you miss the ball
+    //case rolling a 3 = you hit the ball sideways but it's out of the sandpit
+    //case rolling a 4 = you hit the ball onto the green. Good job.
+}
+//10 yards - 0yards
+function PuttingMenu (){
+
+    // the clicker doesn't go away for your putter.
+}
+
+function endMenu (){
+
+ // gives them their score.
+
+}
+
+
